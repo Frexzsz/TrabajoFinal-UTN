500cff71f8d170574c977b8240a394a764e05334
GabrielR711 <gabrielrobles2011@hotmail.com.ar>
2023-04-17 20:35:05 -0300

    0

--- PresentacionPersonal/PresJavs.js
+++ PresentacionPersonal/PresJavs.js
@@ -4,7 +4,7 @@ window.addEventListener("load", function() {
     hamburger = document.querySelector(".hamburger");
     hamburger.onclick = function () {
     navBar = document.querySelector(".nav-bar");
-    navBar.classList.toggle("active");
+    navBar.classList.toggle("nosotros");
 };
 });
 

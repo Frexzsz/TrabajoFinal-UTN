16ae533bd2dcc530b11ecfcee188f0853d924e25
Sofia2255 <sofiamaldonado145@gmail.com>
2023-04-17 14:32:20 -0300

    validacion de datos IMPORTANTE

--- Contacto/index.js
+++ Contacto/index.js
@@ -12,18 +12,59 @@ window.addEventListener("load", function () {
 // ================== form ============================
 // ------------ campos obligatorios--------------
 
+
 function validarForm() {
     var nombre = document.getElementById('imp1').value;
     var email = document.getElementById('imp4').value;
+    var texto= document.getElementById('textof');
+    var entrar=false;
+   texto.innerHTML="";
 
-    if (nombre == "" || email == "") {
-        alert("Por favor, completa todos los campos obligatorios.");
-        return false;
+    if (nombre == "" && email == "") {
+        
+        
+        entrar = true;
+        
+    }
+    else if (nombre == "" && email != ""){
+        
+    
+        entrar = true;
+       
+        
+    }
+    else if (nombre != "" && email == "") {
+        
+        
+        entrar = true;
+
+
     }
     else {
-        return true;
+        texto.innerHTML += "<p>¡Enviado!</p>";
+       
     }
 }
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
 
 
 
